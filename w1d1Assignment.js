ec719abd62ba87737cded152c43e14bd7e256164
satsbeha <79467896+satsbeha@users.noreply.github.com>
2021-02-22 19:05:49 -0800

    Create w1d1Assignment.js

---
+++ w1d1Assignment.js
@@ -0,0 +1,49 @@
+/*
+What happens when an assignment is performed without using let or const keywords in
+strict mode
+
+as javascript is loss type of progrmaing we use strict mode and if we perform without using let or const keyword
+an error will through.
+*/
+
+/*
+List 5 reserved keywords in JavaScript
+do
+break
+for
+case
+export
+yield
+
+*/
+/*
+How is a computer program similar/ different then an algorithm.
+computer program and algorithm are similar in such a way they discribe(instruction) a process step by step (from start to end)
+However, they are diffrent as computer program are  consist of instruction and data where as algorithm conisist of instrucion only.
+
+*/
+/*
+• Write at least two algorithms you perform on your daily life, with at
+least one algorithm that can be converted to computer program.
+
+1.  making coffee
+- turn on the coffee machine
+- Add 2 glass of water
+- Add 3 spoon of coffee
+-wait for 7 minuts
+- turn off the coffee machine
+
+// this algorithim can be conveted to computer program
+
+2. Eating lunch at the cafe
+- hand washing at the gate of the cafe
+-display student  ID
+-  test for  body teprature 
+-having lunch
+- return the lunch box
+- washing hand at the gate
+- exit the cafe
+*/
+
+
+
