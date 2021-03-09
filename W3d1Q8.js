51ea10e3206b554fc6506d8aa4f1de9b6b1a48d7
satsbeha <79467896+satsbeha@users.noreply.github.com>
2021-03-08 21:20:31 -0800

    Create W3d1Q8.js

---
+++ W3d1Q8.js
@@ -0,0 +1,15 @@
+//From Reading:
+//1. How can you make a copy of an object?
+
+//To copy an object in JavaScript, you have three options:
+
+// A. Use the spread (...) syntax
+// B.Use the Object.assign() method
+// C.Use the JSON.stringify() and JSON.parse() methods
+
+
+
+//2. How do you remove the white spaces that may be at the beginning and ending of a string
+//value?
+
+//  The trim() method will remove both leading and trailing whitespace from a string and return the result
